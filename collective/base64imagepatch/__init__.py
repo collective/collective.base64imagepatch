3291083bda6e1d4c77c7c93d01397e595e2da72d
Alexander Loechel <AlexanderLoechel@yahoo.de>
2012-11-20 23:33:17 +0100

    initial version

---
+++ collective/base64imagepatch/__init__.py
@@ -0,0 +1,13 @@
+# -*- extra stuff goes here -*-
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+    
+    
+    
+    
+def patch_base64_images(event):
+    """ Patch created content if it contains an inline images coded as base64 """
+    
+    
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.base64imagepatch',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='zope plone base64 image patcher',
+      author='Alexander Loechel',
+      author_email='Alexander.Loechel@lmu.de',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
