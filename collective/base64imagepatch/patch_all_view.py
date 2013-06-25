c035a95de1b737bb20d05a25b7c37dd9c9ced46c
Alexander Loechel <AlexanderLoechel@yahoo.de>
2013-06-25 20:37:27 +0200

    patch all will now print continous status outputs

--- collective/base64imagepatch/patch_all_view.py
+++ collective/base64imagepatch/patch_all_view.py
@@ -6,59 +6,70 @@ from Products.CMFCore.interfaces import IContentish
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 
-## Patch Browser View for all portals
+
     
 class PatchAllView(BrowserView):
-
+    """
+    Patch Browser View for all portals
+    """
 
     def apply_patch_on_plone_instance(self,portal):
         """ 
         Apply patch on all content object on package installation 
         """
         
-        ## get site_root and portal_catalog
-        logger.info("Patch Plone Site: " + portal.id + 
-                    " at path: " + portal.absolute_url())  
-
         catalog = getToolByName(portal, 'portal_catalog')
 
-        ## query catalog for all content objects that provide IContentish interface
+        ## query catalog for all content objects that 
+        ## provide IContentish interface
         all_objects = catalog(object_provides=IContentish.__identifier__)
 
         ## call patch method for all content objects
         for obj in all_objects:
+            self.request.response.write("Patch Object: " + obj.id \
+                + " at path: " + obj.getPath() + "\n")
+            self.request.response.flush()
+            logger.info("Patch Object: " + obj.id + " at path: " \
+                + obj.getPath())
             patch_object(obj)
+
+    def patch_instance(self, portal):
+        self.request.response.write("Starting patching Plone Instance: " \
+            + portal.id + " at path: " + portal.absolute_url() + "\n")
+        self.request.response.flush()
+        logger.info("Starting patching Plone Instance: " + portal.id \
+            + " at path: " + portal.absolute_url())
+
+        self.apply_patch_on_plone_instance(portal)
+
+        self.request.response.write("Finished patching Plone Instance: " \
+            + portal.id + " at path: " + portal.absolute_url() + "\n\n")
+        self.request.response.flush()
+        logger.info("Finished patching Plone Instance: " + portal.id \
+            + " at path: " + portal.absolute_url() + "\n")
 
     def search(self,context):
         for item in context.values():
             if item.meta_type == "Plone Site":
-                self.apply_patch_on_plone_instance(item)
-                print "\nPlone Instance: " + item.id + " at path: " \
-                    + item.absolute_url() + " patched"
+                self.patch_instance(item)
             if "Folder" in item.meta_type:
                 self.search(item)
 
     def __call__(self):
-        logger.info("Patch All")
+        logger.info("Start Patch All")
 
         context = self.context
 
-        print "Start Patch All Script"
         while not context.isTopLevelPrincipiaApplicationObject:
-            logger.info("current Path: " + context.absolute_url())
+            logger.debug("current Path: " + context.absolute_url())
             if context.meta_type == "Plone Site":
-                self.apply_patch_on_plone_instance(context)
-                print "\nPlone Instance: " + context.id + " at path: " \
-                    + context.absolute_url() + " patched"
+                self.patch_instance(context)
                 break
             else:
                 context = context.getParentNode()
 
         self.search(context)
+        logger.info("Finished Patch All\n\n")
 
-        print "\n\nPatch All finished"
-        return printed
-
-
-
+        return "Finished Patch All"
      
