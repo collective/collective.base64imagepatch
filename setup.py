4383eb51402a365e8598517bdf6f4c0205128adb
Alexander Loechel <AlexanderLoechel@yahoo.de>
2013-06-25 21:47:09 +0200

    doc changed

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.10.dev0'
+version = '0.9'
 
 setup(name='collective.base64imagepatch',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
